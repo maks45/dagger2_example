22b5c4b84fcf953ded70263769ccff762697514e
maks45 <maksim.durov45@gmail.com>
2020-12-21 21:42:46 +0200

    first commit

---
+++ app/src/main/java/com/dagger2example/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dagger2example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.refferertest.dagger2example.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
